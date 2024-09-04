029d4bdc6a18bf54d1d9682507bdeb9294ba4c05
akira <akira@drox.com>
2024-09-04 10:26:17 +0000

    add functions and update readme

---
+++ shakeddb.js
@@ -0,0 +1,14 @@
+
+var a = 10;
+
+function add(a, b) {
+    return a + b;
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+function isEvenOrOdd(number) {
+    return number % 2 === 0 ? 'even' : 'odd';
+}
